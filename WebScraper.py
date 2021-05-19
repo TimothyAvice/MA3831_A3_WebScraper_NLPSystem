03ffc903ea8e06374b81f03fdb174141d619679e
TimothyAvice <timothy.avicedubuisson@my.jcu.edu.au>
2021-05-19 11:42:17 +1000

    Retrieve laptop page hrefs - Used to retrieve extensive specifications
    Set up structure of data storage
    - Defined dictionary tags and sub dictionaries

--- WebScraper.py
+++ WebScraper.py
@@ -9,24 +9,46 @@ from selenium.webdriver.common.keys import Keys
 def main():
     website_url = 'https://laptopmedia.com/au/all-laptop-series/'
 
-    number_of_pages = 50
-
     driver = webdriver.Chrome()
-
-    all_laptops = []
-    all_laptop_info = []
 
     driver.get(website_url)
 
     page_html = driver.page_source
 
-    driver.quit()
-
     page_soup = soup(page_html, 'html.parser')
 
-    laptops = page_soup.findAll("a", {"class":"lp-series-table-row"})
+    links = []
+    for a in page_soup.findAll("a", {"class":"lp-series-table-row"}, href=True):
+        links.append(a['href'])
 
-    print(len(laptops))
+    # For each laptop it will have the following structure
+    #{"Name": [], "Specifications": {"CPU": [{}], "GPU": [{}], "Display": [],
+    # "HDD/SSD": "", "M.2 Slot": "", "RAM": "", "OS": "",
+    # "Dimensions": "", "Weight": "", "Body Material": "", "Ports and conductivity": {}, "Features": {}}}
+
+    # For each CPU
+    # {"Name": "", "Frequency": "", "Cores": "", "Instruction type": "", "TDP": "", "Max Temp": "", "Memory channels": "",
+    # "Released": "", "Architecture": "", "Threads": "", "LL Cache": "", "Lithography": "", "Max GPU Frequency": "",
+    # "Memory Type": "", "Max memory": ""}
+
+    # For each GPU
+    # {"Name": "", "Manufacturing Process": "", "Architecture": "", "Base frequency": "", "Memory Type": "",
+    # "Memory Frequency": "", "Memory bandwidth": "", "Released": "", "Power consumption": "", "Cores": "",
+    # "Maximum frequency": "", "Memory Capacity": "", "Memory bus": ""}
+
+    # For each Display
+    # Direct text copy
+
+    laptops = []
+    for link in links:
+        driver.get(link)
+        page_html = driver.page_source
+        page_soup = soup(page_html, 'html.parser')
+
+
+    CPU_links = []
+    GPU_links = []
+    disp_links = []
 
 
 main()
